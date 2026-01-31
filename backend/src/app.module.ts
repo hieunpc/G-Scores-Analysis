1d82886f622336e459f78499adcf440326450a9b
Hieu NPC <trunghieu_npc@gmail.com>
2026-01-31 08:49:05 +0700

    feat: add module students and its enitty

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentsModule } from './students/students.module';
 
 @Module({
   imports: [
@@ -20,7 +21,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: false,
       }),
       inject: [ConfigService],
-    })
+    }),
+    StudentsModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/students/entities/student.entity.ts
@@ -0,0 +1,37 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity('students')
+export class Student {
+  @PrimaryColumn({ type: 'varchar', length: 8 })
+  sbd: string;
+
+  @Column({ type: 'decimal', precision: 3, scale: 2, nullable: true })
+  toan: number | null;
+
+  @Column({ type: 'decimal', precision: 3, scale: 2, nullable: true })
+  ngu_van: number | null;
+
+  @Column({ type: 'decimal', precision: 3, scale: 2, nullable: true })
+  ngoai_ngu: number | null;
+
+  @Column({ type: 'decimal', precision: 3, scale: 2, nullable: true })
+  vat_li: number | null;
+
+  @Column({ type: 'decimal', precision: 3, scale: 2, nullable: true })
+  hoa_hoc: number | null;
+
+  @Column({ type: 'decimal', precision: 3, scale: 2, nullable: true })
+  sinh_hoc: number | null;
+
+  @Column({ type: 'decimal', precision: 3, scale: 2, nullable: true })
+  lich_su: number | null;
+
+  @Column({ type: 'decimal', precision: 3, scale: 2, nullable: true })
+  dia_li: number | null;
+
+  @Column({ type: 'decimal', precision: 3, scale: 2, nullable: true })
+  gdcd: number | null;
+
+  @Column({ type: 'varchar', length: 2, nullable: true })
+  ma_ngoai_ngu: string | null;
+}
