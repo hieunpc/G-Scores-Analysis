075e6f86182135a8760ec4bf6c668f539bc64ea5
Hieu NPC <trunghieu_npc@gmail.com>
2026-02-01 20:39:59 +0700

    chore: add docker config and run with docker

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import { SUBJECTS, SUBJECT_GROUPS } from '../constants';
 
 const Dashboard = () => {
 
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import type { Student } from '../types/student.type';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.VITE_API_URL || '/api';
 
 const handleResponse = async (response: Response) => {
   if (!response.ok) {
